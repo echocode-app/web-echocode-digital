ac222d15742d54ae1e92f23bbdb3fd0ffd7cea44
BorovChe <roman2000borov@gmail.com>
2026-02-23 20:40:37 +0200

    EED-41 [FE] Multi-layered Armor

--- src/app/(public)/service-direction/qa/page.tsx
+++ src/app/(public)/service-direction/qa/page.tsx
@@ -1,7 +1,8 @@
+import StaticGradientLine from '@/components/UI/StaticGradientLine';
+import ArmorSection from '@/components/sections/directions/qa/ArmorSection';
 import BusinessSection from '@/components/sections/directions/qa/BusinessSection';
 import HeroSection from '@/components/sections/directions/qa/HeroSection';
 import ModerationSection from '@/components/sections/directions/qa/ModerationSection';
-import StaticGradientLine from '@/components/UI/StaticGradientLine';
 
 const QAPage = () => {
   return (
@@ -10,6 +11,7 @@ const QAPage = () => {
       <StaticGradientLine />
       <BusinessSection />
       <ModerationSection />
+      <ArmorSection />
     </>
   );
 };
