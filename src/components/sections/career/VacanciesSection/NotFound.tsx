9bf5eea1f3fcfe63bd74d358a9d2ac71a8958af3
Roman <120989445+BorovChe@users.noreply.github.com>
2026-03-15 20:49:19 +0200

    fix error localization

--- src/components/sections/career/VacanciesSection/NotFound.tsx
+++ src/components/sections/career/VacanciesSection/NotFound.tsx
@@ -1,12 +1,15 @@
+import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 
 const NotFound = () => {
+  const t = useTranslations('VacancyError');
+
   return (
     <div className="flex flex-col md:flex-row items-center justify-center gap-4 md:gap-16">
       <div>
-        <h3 className="font-title text-title-2xl mb-2">No open positions</h3>
+        <h3 className="font-title text-title-2xl mb-2 uppercase">{t('title')}</h3>
         <p className="max-w-90 mb-4  text-left text-main-sm md:text-main-base text-gray75">
-          We’re not hiring right now, but new opportunities may open soon.
+          {t('description')}
         </p>
       </div>
       <div className="w-65.5 h-65.5 relative">
--- src/components/sections/portfolio/PortfolioSection/NotFound.tsx
+++ src/components/sections/portfolio/PortfolioSection/NotFound.tsx
@@ -1,20 +1,24 @@
+import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const NotFound = () => {
+  const t = useTranslations('PortfolioError');
+
   return (
     <div className="flex flex-col md:flex-row items-center justify-center gap-4 md:gap-16 pt-14">
       <div>
-        <h3 className="font-title text-title-2xl mb-2">No projects found</h3>
+        <h3 className="font-title text-title-2xl mb-2 uppercase">{t('title')}</h3>
         <p className="mb-4 text-center md:text-left text-main-sm md:text-main-base text-gray75">
-          Try changing your filters.
+          {t('description')}
         </p>
         <Link
           href={'/portfolio'}
           scroll={false}
-          className="block px-4 py-2 mx-auto md:mx-0 w-fit text-title-xs font-title bg-accent rounded-secondary hover:bg-accent/90 duration-main"
+          className="block px-4 py-2 mx-auto md:mx-0 w-fit text-title-xs font-title bg-accent rounded-secondary
+           hover:bg-accent/90 duration-main uppercase"
         >
-          Reset Filters
+          {t('resetButton')}
         </Link>
       </div>
       <div className="w-65.5 h-65.5 relative">
