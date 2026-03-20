759728378dfe32ebc01a79455083a695b2f04538
AnnaK <kotlyaranya1771@gmail.com>
2026-03-20 15:27:17 +0200

    gradient line & portfolio grid

--- src/components/UI/section/SectionGradientLine.tsx
+++ src/components/UI/section/SectionGradientLine.tsx
@@ -1,12 +1,17 @@
 interface SectionGradientLineProps {
   height: string;
+  fullWidth?: boolean;
 }
 
-const SectionGradientLine = ({ height }: SectionGradientLineProps) => {
+const SectionGradientLine = ({ height, fullWidth = false }: SectionGradientLineProps) => {
   return (
-    <div className="md:px-8 mb-6">
+    <div className={fullWidth ? 'mb-6' : 'md:px-8 mb-6'}>
       <div
-        className="bg-section-gradient-animated w-full max-w-250 mx-auto"
+        className={
+          fullWidth
+            ? 'bg-section-gradient-animated w-full'
+            : 'bg-section-gradient-animated w-full max-w-250 mx-auto'
+        }
         style={{ height: `${height}px` }}
       />
     </div>
