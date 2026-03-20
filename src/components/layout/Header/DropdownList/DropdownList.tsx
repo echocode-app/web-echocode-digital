8c4f79a01540f18b418a3fac0a72e0e7374234f9
AnnaK <kotlyaranya1771@gmail.com>
2026-03-21 00:23:27 +0200

    Update DropdownList.tsx

--- src/components/layout/Header/DropdownList/DropdownList.tsx
+++ src/components/layout/Header/DropdownList/DropdownList.tsx
@@ -5,14 +5,16 @@ const DropdownList = () => {
     <div
       className="
        absolute left-1/2 top-full pt-7.25 -translate-x-56
-       opacity-0  group-hover:opacity-100  pointer-events-none group-hover:pointer-events-auto
-       z-50"
+       z-50 transition-all duration-main
+       -translate-y-2 opacity-0 pointer-events-none
+       group-hover:translate-y-0 group-hover:opacity-100 group-hover:pointer-events-auto"
     >
       <ul
         className="
       pointer-events-none
       transition-all duration-main
-      opacity-0  group-hover:opacity-100
+      opacity-0
+      group-hover:opacity-100
       group-hover:pointer-events-auto
       flex mx-auto md:w-[710] lg:w-198.75  md:gap-2 lg:gap-3 p-3 border rounded-secondary
      border-gray10 bg-black/15 backdrop-blur-[26px]"
