b682f6e2efd428c86a7dcb9ed39e88f962133bf6
Roman <120989445+BorovChe@users.noreply.github.com>
2026-03-22 21:33:29 +0200

    update logo

--- src/components/layout/Header/HeaderLogo.tsx
+++ src/components/layout/Header/HeaderLogo.tsx
@@ -9,7 +9,7 @@ const HeaderLogo = () => {
   const scrollToTop = useScrollToTop('/');
 
   return (
-    <Link href={'/'} className="flex items-center lg:mr-9.25 gap-3 z-10" onClick={scrollToTop}>
+    <Link href={'/'} className="flex items-center lg:mr-9.25 gap-6 z-10" onClick={scrollToTop}>
       <Logo />
       <p className="font-wadik hidden xl:block text-title-xs uppercase">echocode</p>
     </Link>
