47c033326b664ef4c66e5531c108b9d371c641ab
AnnaK <kotlyaranya1771@gmail.com>
2026-03-24 12:58:53 +0200

    upgrade social links in FOOTER
    add WhatsApp
    delite viber😅

--- src/components/layout/Footer/FooterNavigation/SocialLinks.tsx
+++ src/components/layout/Footer/FooterNavigation/SocialLinks.tsx
@@ -6,10 +6,21 @@ const SocailLinks = () => {
     <ul className="flex justify-center gap-2 lg:justify-end">
       <li>
         <Link
+          href={'http://instagram.com/echocode.app'}
+          target="blank"
+          className="flex justify-center items-center w-10 h-10 rounded-full bg-social-instagram-link"
+        >
+          <div className="relative w-4.5 h-4.5">
+            <Image src="/UI/social-icons/insta.svg" fill alt="Instagram" />
+          </div>
+        </Link>
+      </li>
+      <li>
+        <Link
           href={'https://t.me/echocode_app'}
           target="blank"
-          className="flex justify-center items-center w-10 h-10 bg-base-gray rounded-full 
-          hover:bg-accent duration-main"
+          className="flex justify-center items-center w-10 h-10 rounded-full bg-social-telegram 
+          hover:bg-accent focus-visible:bg-accent duration-main"
         >
           <div className="relative w-4.5 h-3.5">
             <Image src="/UI/social-icons/telegram.svg" fill alt="Telegram" />
@@ -18,13 +29,13 @@ const SocailLinks = () => {
       </li>
       <li>
         <Link
-          href={'http://instagram.com/echocode.app'}
+          href={'https://wa.me/34683581566'}
           target="blank"
-          className="flex justify-center items-center w-10 h-10 bg-base-gray rounded-full
-          hover:bg-accent duration-main"
+          className="flex justify-center items-center w-10 h-10 rounded-full bg-social-whatsapp 
+          hover:bg-accent focus-visible:bg-accent duration-main"
         >
           <div className="relative w-4.5 h-4.5">
-            <Image src="/UI/social-icons/insta.svg" fill alt="Instagram" />
+            <Image src="/UI/social-icons/whatsapp.svg" fill alt="WhatsApp" />
           </div>
         </Link>
       </li>
